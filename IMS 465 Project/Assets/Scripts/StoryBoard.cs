873531b0b355f9f227be9bf49f2c9d79416db074
MaxVendely <maxvendely@gmail.com>
2024-10-24 18:21:02 -0400

    Added slideshow intro scene

---
+++ IMS 465 Project/Assets/Scripts/StoryBoard.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryBoard : MonoBehaviour
+{
+    public GameObject[] slides;
+
+    public GameObject finalSlide;
+
+    void Start()
+    {
+        StartCoroutine(SlideShow());
+    }
+
+    IEnumerator SlideShow()
+    {
+        foreach (GameObject slide in slides)
+        {
+            slide.SetActive(true);
+
+            yield return new WaitForSeconds(3);
+
+            slide.SetActive(false);
+        }
+
+        finalSlide.SetActive(true);
+    }
+}
